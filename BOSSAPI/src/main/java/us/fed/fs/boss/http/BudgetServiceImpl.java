71d53adbd91f0db4a46755a730a08cccb3b7716b
bhowden <bhowden@fs.fed.us>
2018-05-22 16:24:07 -0400

    expense cols

--- BOSSAPI/src/main/java/us/fed/fs/boss/http/BudgetServiceImpl.java
+++ BOSSAPI/src/main/java/us/fed/fs/boss/http/BudgetServiceImpl.java
@@ -2,9 +2,12 @@ package us.fed.fs.boss.http;
 
 import java.util.ArrayList;
 import java.util.List;
+import static org.aspectj.weaver.Shadow.ExceptionHandler;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import us.fed.fs.boss.exception.BadRequestException;
+import us.fed.fs.boss.exception.ResourceNotFoundException;
 
 import us.fed.fs.boss.model.Expense;
 import us.fed.fs.boss.repository.ExpenseRepository;
@@ -14,6 +17,18 @@ public class BudgetServiceImpl implements BudgetService {
 
     @Autowired
     private ExpenseRepository expenseRepository;
+    
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleResourceNotFoundException() {
+        return "meters/notfound";
+    }
+    
+    @ExceptionHandler(BadRequest Exception.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleResouFoundException() {
+        return "meters/notfound";
+    }
 
     public void addExpense(Expense expense) {
         expenseRepository.save(expense);
--- BOSSAPI/src/main/java/us/fed/fs/boss/model/Expense.java
+++ BOSSAPI/src/main/java/us/fed/fs/boss/model/Expense.java
@@ -1,11 +1,14 @@
 package us.fed.fs.boss.model;
 
+import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 public class Expense {
@@ -14,6 +17,31 @@ public class Expense {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "expense_Sequence")
     @SequenceGenerator(name = "expense_Sequence", sequenceName = "EXP_SEQ")
     private Long id;
+    
+    @Column(name = "ACTCode")
+    private String actCode;
+    
+    @Column(name = "SECCode")
+    private String secCode;
+    
+    @Column(name = "NameCode")
+    private String nameCode;
+    
+    @Column(name = "PayPeriod")
+    private int payPeriod;
+    
+    @Column(name = "JobCode")
+    private int jobCode;
+    
+    @Column(name = "ExpenseCode")
+    private int ExpenseCode;
+    
+    @Column(name = "Total")
+    private long total;
+    
+    @Temporal(TemporalType.DATE)
+    @Column(name = "DateObl")
+    private Date obligatedDate;
 
     @Column(name = "description")
     private String description;
@@ -44,6 +72,118 @@ public class Expense {
      */
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    /**
+     * @return the actCode
+     */
+    public String getActCode() {
+        return actCode;
+    }
+
+    /**
+     * @param actCode the actCode to set
+     */
+    public void setActCode(String actCode) {
+        this.actCode = actCode;
+    }
+
+    /**
+     * @return the secCode
+     */
+    public String getSecCode() {
+        return secCode;
+    }
+
+    /**
+     * @param secCode the secCode to set
+     */
+    public void setSecCode(String secCode) {
+        this.secCode = secCode;
+    }
+
+    /**
+     * @return the nameCode
+     */
+    public String getNameCode() {
+        return nameCode;
+    }
+
+    /**
+     * @param nameCode the nameCode to set
+     */
+    public void setNameCode(String nameCode) {
+        this.nameCode = nameCode;
+    }
+
+    /**
+     * @return the payPeriod
+     */
+    public int getPayPeriod() {
+        return payPeriod;
+    }
+
+    /**
+     * @param payPeriod the payPeriod to set
+     */
+    public void setPayPeriod(int payPeriod) {
+        this.payPeriod = payPeriod;
+    }
+
+    /**
+     * @return the jobCode
+     */
+    public int getJobCode() {
+        return jobCode;
+    }
+
+    /**
+     * @param jobCode the jobCode to set
+     */
+    public void setJobCode(int jobCode) {
+        this.jobCode = jobCode;
+    }
+
+    /**
+     * @return the ExpenseCode
+     */
+    public int getExpenseCode() {
+        return ExpenseCode;
+    }
+
+    /**
+     * @param ExpenseCode the ExpenseCode to set
+     */
+    public void setExpenseCode(int ExpenseCode) {
+        this.ExpenseCode = ExpenseCode;
+    }
+
+    /**
+     * @return the total
+     */
+    public long getTotal() {
+        return total;
+    }
+
+    /**
+     * @param total the total to set
+     */
+    public void setTotal(long total) {
+        this.total = total;
+    }
+
+    /**
+     * @return the obligatedDate
+     */
+    public Date getObligatedDate() {
+        return obligatedDate;
+    }
+
+    /**
+     * @param obligatedDate the obligatedDate to set
+     */
+    public void setObligatedDate(Date obligatedDate) {
+        this.obligatedDate = obligatedDate;
     }
 
 }
