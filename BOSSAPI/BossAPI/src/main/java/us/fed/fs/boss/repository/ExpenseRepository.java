9a9708992a8d84a3eac808dfa4d0bd0944159da7
bhowden <bhowden@fs.fed.us>
2018-06-08 11:58:57 -0400

    enable caching

--- BOSSAPI/BossAPI/src/main/java/us/fed/fs/boss/repository/ExpenseRepository.java
+++ BOSSAPI/BossAPI/src/main/java/us/fed/fs/boss/repository/ExpenseRepository.java
@@ -1,10 +1,26 @@
 package us.fed.fs.boss.repository;
 
+import java.util.List;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import us.fed.fs.boss.model.Expense;
 
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
-  // ...
+
+    @Override
+    @CachePut("expenses")
+    <S extends Expense> S save(S entity);
+
+    @Override
+    @Cacheable("expenses")
+    List<Expense> findAll();
+
+    @Override
+    @CacheEvict("expenses")
+    void delete(Expense expense);
+
 }
