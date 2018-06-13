1d267506af363c18805f4e62b8803962468feaef
Mike Laney <mlaney@cynerge.com>
2018-06-13 16:11:52 -0400

    initial table styling

--- BOSSUI/js/pagesJS/viewExpense.js
+++ BOSSUI/js/pagesJS/viewExpense.js
@@ -3,47 +3,94 @@ var api = "http://localhost:8090/expense";
 
 // NEED TO CHANGE BACK WHEN DONE TESTING
 
+var dataSet = [
+    [ "S", "AD", "LaneyM", "UPS", "05", "2019", "FRF13818", "26", "12,000,000", "2011/04/25" ],
+    [ "FS", "HD", "LaneyM", "FEDEX", "05","2019", "FRF13818", "26", "12,000,000", "2011/07/25" ],
+    [ "SF", "TD", "LaneyM", "UPS", "05", "2019","FRF13818", "26", "1,877", "2011/07/25" ],
+    [ "DS", "GD", "LaneyM", "AZ", "05", "2019","FRF13818", "26", "12,000,000", "2011/07/25" ],
+    [ "RS", "AD", "LaneyM", "UPS", "05", "2019","FRF13818", "26", "100", "2011/07/25" ],
+    [ "R", "DD", "LaneyM", "DISCOUNT STORAGE", "05", "2016","FRF13818", "26", "12,000", "2011/07/25" ],
+    [ "K", "D", "LaneyM", "UPS", "05", "2019","FRF13818", "26", "12,000,000", "2011/07/25" ],
+
+];
+
 $(document).ready(function() {
 
     $('#expense').DataTable({  
-        // "bPaginate": false,
-        // "bfilter": false,
-        // "bInfo": false,
-        // "bSearch": false,
+        dom: 'Bfrtip',
         bProcessing: true,
-        ajax: {
-            url : "http://localhost:8090/expense", "dataSrc": ""
-        },
+        // ajax: {
+        //     url : "http://localhost:8090/expense", "dataSrc": dataSet
+        // },
+        buttons: [
+            'print'
+        ],
+        data: dataSet,
+        "columnDefs": 
+        {"width": "40%", "targets": "3"},
 
-        columns:[
-            {data: "actCode"},       
-            {data: "activityCode"},
-            {data: "employeeProfile.nameCode"},
-            {data: "description"},
-            {data: "payPeriod"},
-            {data: "id"},
-            {data: "jobCode.jobCode"},
-            {data: "expenseCode"},
+        // columns:[
+        //     {data: "actCode"},       
+        //     {data: "activityCode"},
+        //     {data: "employeeProfile.nameCode"},
+        //     {data: "description"},
+        //     {data: "payPeriod"},
+        //     {data: "id"},
+        //     {data: "jobCode.jobCode"},
+        //     {data: "expenseCode"},
+        //     {data: null,
+        //         "render": function(){
+        //             return 'Total'
+        //         }
+        //     },
+        //     {data: "obligatedDate"},
+        //     {data: null,
+        //         "render": function(){
+        //             return 'Elip'
+        //         }
+        //     },
+        //     {data: null,
+        //         "render": function(){
+        //             return 'Select'
+        //         }
+        //     }],
+        columns: [
+            { title: "Act Code" },
+            { title: "Sec Code" },
+            { title: "Name Code" },
+            { title: "Description" },
+            { title: "Pay Period" },
+            { title: "Seq #." },
+            { title: "Job Code" },
+            { title: "Exp Code" },
+            { title: "Total" },
+            { title: "Date Obl" },
             {data: null,
                 "render": function(){
-                    return 'Total'
+                    return '<i class="fa fa-ellipsis-v"></i>'
                 }
             },
-            {data: "obligatedDate"},
             {data: null,
                 "render": function(){
-                    return 'Elip'
-                }
-            },
-            {data: null,
-                "render": function(){
-                    return 'Select'
+                    return '<i class="fa fa-clipboard" style="color:#006633"></i>'
                 }
             }],
+        
         })
     });
 
-
+    // <th scope="col">Act Code</th>
+    // <th scope="col">Sec Code</th>
+    // <th scope="col">Name Code</th>
+    // <th scope="col">Description</th>
+    // <th scope="col">Pay Period</th>
+    // <th scope="col">Seq. #</th>
+    // <th scope="col">Job Code</th>
+    // <th scope="col">Exp Code</th>
+    // <th scope="col">Total</th>
+    // <th scope="col">Date Obl</th>
+    // <th scope="col">Elipsis</th>
+    // <th scope="col">Select</th>  
 
 
 
