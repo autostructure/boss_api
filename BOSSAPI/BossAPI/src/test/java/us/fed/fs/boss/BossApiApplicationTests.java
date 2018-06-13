b18b7c8880b9d31f121c40e011395d07e7d08dbe
bhowden <bhowden@fs.fed.us>
2018-06-12 23:58:21 -0400

    change property name to match mockup for ExpenseCode.type

---
+++ BOSSAPI/BossAPI/src/test/java/us/fed/fs/boss/BossApiApplicationTests.java
@@ -0,0 +1,37 @@
+package us.fed.fs.boss;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BossApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+            System.out.println("********************** TEST **********************");
+            System.out.println("********************** TEST **********************");
+            System.out.println("********************** TEST **********************");
+            System.out.println("********************** TEST **********************");
+            System.out.println("********************** TEST **********************");
+            System.out.println("********************** TEST **********************");
+            System.out.println("********************** TEST **********************");
+            System.out.println("********************** TEST **********************");
+            System.out.println("********************** TEST **********************");
+            System.out.println("********************** TEST **********************");
+            System.out.println("********************** TEST **********************");
+            System.out.println("********************** TEST **********************");
+            System.out.println("********************** TEST **********************");
+            System.out.println("********************** TEST **********************");
+            System.out.println("********************** TEST **********************");
+            System.out.println("********************** TEST **********************");
+            System.out.println("********************** TEST **********************");
+            System.out.println("********************** TEST **********************");
+            System.out.println("********************** TEST **********************");
+            System.out.println("********************** TEST **********************");
+            System.out.println("********************** TEST **********************");
+	}
+
+}
