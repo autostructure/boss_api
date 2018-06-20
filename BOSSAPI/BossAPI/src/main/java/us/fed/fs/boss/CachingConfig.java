8aa37a051b70d907614aee13abd8769a6fb2bc8a
bhowden <bhowden@fs.fed.us>
2018-06-20 14:01:39 -0400

    revert cache creation to eager to speed up local dev

--- BOSSAPI/BossAPI/src/main/java/us/fed/fs/boss/CachingConfig.java
+++ BOSSAPI/BossAPI/src/main/java/us/fed/fs/boss/CachingConfig.java
@@ -12,6 +12,7 @@ public class CachingConfig {
 
     @Bean
     public CacheManager cacheManager() {
-        return new ConcurrentMapCacheManager();
+         return new ConcurrentMapCacheManager("expenses", "jobCodes", "activityCodes",
+         "budgetObjectCodes", "expenseCodes", "paymentCodes", "employeeProfiles");
     }
 }
