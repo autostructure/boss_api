895d2c83ff088085ba0f258a6504aff3702d8004
bhowden <bhowden@fs.fed.us>
2018-06-22 15:40:11 -0400

    complete crud unit tests for /jobCode

--- BOSSAPI/BossAPI/src/test/java/us/fed/fs/boss/BossApiApplicationTests.java
+++ BOSSAPI/BossAPI/src/test/java/us/fed/fs/boss/BossApiApplicationTests.java
@@ -4,12 +4,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
@@ -37,11 +40,30 @@ public class BossApiApplicationTests {
     public void jobCodes() {
 
         try {
-            CloseableHttpClient client = HttpClients.createDefault();
-            
-            HttpPost httpPost = new HttpPost(baseUrl + "/jobCode");
             
             ObjectMapper objectMapper = new ObjectMapper();
+            CloseableHttpClient client = HttpClients.createDefault();
+            
+            
+            // READ
+            
+            HttpGet httpGet = new HttpGet(baseUrl + "/jobCode");
+            httpGet.setHeader("Content-type", "application/json");
+            CloseableHttpResponse getRes = client.execute(httpGet);
+            HttpEntity getResEnt = getRes.getEntity();
+            String responseStringGET = EntityUtils.toString(getResEnt, "UTF-8");
+            Integer getstatus = getRes.getStatusLine().getStatusCode();
+            System.out.println("*****************************GET /jobCode *********************************");
+            System.out.println(getstatus);
+            System.out.println("****************************************************************************");
+            Assert.assertEquals(getstatus.intValue(), 200);
+            List<JobCode> jobCodes = objectMapper.readValue(responseStringGET, List.class);
+            
+            Assert.assertTrue(jobCodes.size() > 0);
+            
+            // CREATE
+            
+            HttpPost httpPost = new HttpPost(baseUrl + "/jobCode");
             
             JobCode jobCode = new JobCode();
             jobCode.setJobCode("TESST123");
@@ -65,13 +87,53 @@ public class BossApiApplicationTests {
             System.out.println("****************************************************************************");
             Assert.assertEquals(response.getStatusLine().getStatusCode(), 200);
             
+            // UPDATE
+            HttpPut httpPut = new HttpPut(baseUrl + "/jobCode/" + saved.getId());
+            
+            saved.setDescription("PutTested");
+            
+            String putBody = objectMapper.writeValueAsString(saved);
+            StringEntity putEntity = new StringEntity(putBody);
+            httpPut.setEntity(putEntity);
+            httpPut.setHeader("Accept", "application/json");
+            httpPut.setHeader("Content-type", "application/json");
+            CloseableHttpResponse putResponse = client.execute(httpPut);
+            Integer putStatus = putResponse.getStatusLine().getStatusCode();
+            System.out.println();
+            System.out.println("*****************************PUT /jobCode *********************************");
+            System.out.println(putStatus);
+            System.out.println("****************************************************************************");
+            System.out.println();
+            Assert.assertEquals(putStatus.intValue(), 200);
+            
+            // READ SINGLE AND ASSERT UPDATE WORKED
+            
+            HttpGet singleHttpGet = new HttpGet(baseUrl + "/jobCode/" + saved.getId());
+            singleHttpGet.setHeader("Content-type", "application/json");
+            CloseableHttpResponse singleRes = client.execute(singleHttpGet);
+            HttpEntity singleGetResEnt = singleRes.getEntity();
+            String singleResponseStringGET = EntityUtils.toString(singleGetResEnt, "UTF-8");
+            Integer singlegetstatus = singleRes.getStatusLine().getStatusCode();
+            System.out.println();
+            System.out.println("*****************************GET /jobCode/"+saved.getId()+" *********************************");
+            System.out.println(singleResponseStringGET);
+            System.out.println("****************************************************************************");
+            System.out.println();
+            Assert.assertEquals(singlegetstatus.intValue(), 200);
+            JobCode updated = objectMapper.readValue(singleResponseStringGET, JobCode.class);
+            
+            Assert.assertTrue(updated.getDescription().equals("PutTested"));
+            
+            // DELETE
+            
             HttpDelete httpDelete = new HttpDelete(baseUrl + "/jobCode/" + saved.getId());
             CloseableHttpResponse delRes = client.execute(httpDelete);
-            
-            System.out.println("*****************************POST /jobCode/"+saved.getId()+" *****************************");
+            System.out.println();
+            System.out.println("*****************************DELETE /jobCode/"+saved.getId()+" *****************************");
             Integer statusdel = delRes.getStatusLine().getStatusCode();
             System.out.println("delete returns https status: " + statusdel);
             System.out.println("****************************************************************************");
+            System.out.println();
             Assert.assertEquals(response.getStatusLine().getStatusCode(), 200);
             
             client.close();
