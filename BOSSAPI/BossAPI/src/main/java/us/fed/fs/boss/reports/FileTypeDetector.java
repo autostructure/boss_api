a4b07813415659ed417e6c31aff677d2aadf9129
bhowden <bhowden@fs.fed.us>
2018-06-29 13:11:52 -0400

    web resources in spring boot project for now

---
+++ BOSSAPI/BossAPI/src/main/java/us/fed/fs/boss/reports/FileTypeDetector.java
@@ -0,0 +1,18 @@
+package us.fed.fs.boss.reports;
+
+import javax.servlet.ServletContext;
+ 
+import org.springframework.http.MediaType;
+ 
+public class FileTypeDetector {
+    public static MediaType getMediaTypeForFileName(ServletContext servletContext, String fileName) {
+        String mineType = servletContext.getMimeType(fileName);
+        try {
+            MediaType mediaType = MediaType.parseMediaType(mineType);
+            return mediaType;
+        } catch (Exception e) {
+            return MediaType.APPLICATION_OCTET_STREAM;
+        }
+    }
+     
+}
