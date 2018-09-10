e61526a0b193283d9ab7ed4b4ad83ebaf4d775b4
bhowden@fs.fed.us <12001003515677@FEDIDCARD.GOV>
2018-09-10 13:00:31 -0400

    duty stations

---
+++ src/main/java/us/fed/fs/boss/repository/DutyStationRepository.java
@@ -0,0 +1,22 @@
+package us.fed.fs.boss.repository;
+
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import us.fed.fs.boss.model.DutyStation;
+
+@Repository
+public interface DutyStationRepository extends JpaRepository<DutyStation, Long> {
+
+    @Override
+    <S extends DutyStation> S save(S entity);
+
+    @Override
+    List<DutyStation> findAll();
+
+    @Override
+    void delete(DutyStation budgetObjectCode);
+
+}
+
