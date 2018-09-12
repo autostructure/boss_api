60b7c2f5444d23c33c95c54590ac55075ce2e31b
bhowden@fs.fed.us <12001003515677@FEDIDCARD.GOV>
2018-09-12 02:17:24 -0400

    dra database code. dra repos and services. protect PII from employeeProfile at the serialization layer for non admin related CRUDS

---
+++ src/main/java/us/fed/fs/boss/repository/DeliberativeRiskAssessmentAircraftRepository.java
@@ -0,0 +1,21 @@
+package us.fed.fs.boss.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import us.fed.fs.boss.model.DeliberativeRiskAssessmentAircraft;
+
+@Repository
+public interface DeliberativeRiskAssessmentAircraftRepository extends JpaRepository<DeliberativeRiskAssessmentAircraft, Long> {
+
+    @Override
+    <S extends DeliberativeRiskAssessmentAircraft> S save(S entity);
+
+    @Override
+    List<DeliberativeRiskAssessmentAircraft> findAll();
+
+    @Override
+    void delete(DeliberativeRiskAssessmentAircraft deliberativeRiskAssessmentAircraft);
+    
+}
+
