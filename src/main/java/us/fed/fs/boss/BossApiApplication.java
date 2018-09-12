c55e9a9abb62387aea058175cbf44cc9795724ed
bhowden@fs.fed.us <12001003515677@fedidcard.gov>
2018-09-12 03:43:27 -0400

    format

--- src/main/java/us/fed/fs/boss/BossApiApplication.java
+++ src/main/java/us/fed/fs/boss/BossApiApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages={
 "us.fed.fs.boss", "us.fed.fs.boss.repository"})
 public class BossApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BossApiApplication.class, args);
 	}
