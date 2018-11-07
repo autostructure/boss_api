bfac937451f42e6b0d521744a99be9dfb2b34d28
Mike Laney <mlaney@cynerge.com>
2018-11-07 13:19:49 -0500

    view Fleet add Accessory2 and Camera

--- src/main/resources/static/js/pagesJS/viewEditFleet.js
+++ src/main/resources/static/js/pagesJS/viewEditFleet.js
@@ -57,42 +57,44 @@ url = '/vehicle/' + id;
             e.preventDefault();
         }
 
-
+        
         // creating vars to submit to ajax
         var form = $('#formGeneralInfo');
         var method = "PUT";
         var url =  '/vehicle/' + id;
         var data = {
-            'license':form.find('[name=license]').val(),
-            'vin':form.find('[name=vin]').val(),
-            'modelYear':form.find('[name=modelYear]').val(),
-            'make':form.find('[name=make]').val(),
-            'modelNumber':form.find('[name=modelNumber]').val(),
-            'description':form.find('[name=description]').val(),
-            'color':form.find('[name=color]').val(),
-            'engineNumber':form.find('[name=engineNumber]').val(),
-            'accessory':form.find('[name=accessory]').val(),
-            'accessory2':form.find('[name=accessory2]').val(),
-            'camera':form.find('[name=camera]').val(),
-            'ownershipType':form.find('[name=ownershipType]').val(),
-            'equipmentNumber':form.find('[name=equipmentNumber]').val(),
-            'vehicleClassCode':form.find('[name=vehicleClassCode]').val(),
-            'state':form.find('[name=state]').val(),
-            'cityOrLocation':form.find('[name=cityOrLocation]').val(),
-            'keysToolBox':form.find('[name=keysToolBox]').val(),
-            'tailgate':form.find('[name=tailgate]').val(),
-            'topper':form.find('[name=topper]').val(),
-            'tonneau':form.find('[name=tonneau]').val(),
-            'dateAquired':form.find('[name=dateAquired]').val(),
-            'replacementDate':form.find('[name=replacementDate]').val(),
-            'disposalDate':form.find('[name=disposalDate]').val(),
-            'releasedDate':form.find('[name=releasedDate]').val(),
-            'assignedOperator':form.find('[name=assignedOperator]').val(),
-        }
+            'license': form.find('[name=license]').val(),
+            'vin': form.find('[name=vin]').val(),
+            'modelYear': form.find('[name=modelYear]').val(),
+            'make': form.find('[name=make]').val(),
+            'modelNumber': form.find('[name=modelNumber]').val(),
+            'description': form.find('[name=description]').val(),
+            'color': form.find('[name=color]').val(),
+            'engineNumber': form.find('[name=engineNumber]').val(),
+            'accessory': form.find('[name=accessory]').val(),
+            'accessory2': form.find('[name=accessory2]').val(),
+            'camera': form.find('[name=camera]').val(),
+            'oldLicense': form.find('[name=oldLicense]').val(),
+            'ownershipType': form.find('[name=ownershipType]').val(),
+            'equipmentNumber': form.find('[name=equipmentNumber]').val(),
+            'vehicleClassCode': form.find('[name=vehicleClassCode]').val(),
+            'state': form.find('[name=state]').val(),
+            'cityOrLocation': form.find('[name=cityOrLocation]').val(),
+            'keysToolBox': form.find('[name=keysToolBox]').val(),
+            'tailgate': form.find('[name=tailgate]').val(),
+            'topper': form.find('[name=topper]').val(),
+            'tonneau': form.find('[name=tonneau]').val(),
+            'dateAquired': getCorrectDateFormat(form.find('[name=dateAquired]').val()),
+            'replacementDate': getCorrectDateFormat(form.find('[name=replacementDate]').val()),
+            'disposalDate': getCorrectDateFormat(form.find('[name=disposalDate]').val()),
+            'releasedDate': getCorrectDateFormat(form.find('[name=releasedDate]').val()),
+            'assignedOperator': form.find('[name=assignedOperator]').val(),
+            'id': parseInt(id)
+        };
         // stringifying the data for ajax
         data = JSON.stringify(data);
-        console.log("current put url is " + url)
-
+        console.log("current put url is " + url);
+        debugger;
         // ajax post call
         $.ajax({
             url: url,
@@ -104,19 +106,25 @@ url = '/vehicle/' + id;
             success: function(response){
                 $('#exampleModal').modal('show');
                 console.log(" *** successfully updated see data below ***");
-                console.log(data);
+                console.log(response);
             },
-            error: function(error){
-                return false;
+            error: function(a,b,c){
+                
                 e.preventDefault();
-                console.log(" !!! Error" + error + " !!! ") ;
+                console.log(" !!! Error" + b + " !!! ");
                 console.log(a.responseJSON);
+                console.log(c);
+
             }
         });
     });
 
 
+function getCorrectDateFormat(date_str) {
+    var date = new Date(date_str);
 
+    return date.toISOString();
+}
 
 // filling out data from form via get
 var data = {};
@@ -141,8 +149,7 @@ forms.find('input:not([disabled]):not([type=submit]), textarea, select').each(fu
 });
 
 $('#monthlyUsage').on('click', function(){
-    // window.location.assign('/viewEditFleet/'+ id + '/iwfia');
-    window.location.assign('/iwfia');
+    window.location.assign('/viewEditFleet/'+ id + '/iwfia');
 })
 
 // generating the fields w/ bootstrap field writer
@@ -247,7 +254,7 @@ var fields = {
                 "After Market": "After Market",
                 "None": "None"
             },         
-        }
+        }        
     ],
     [
         {
