27eba3c70c85bd96f1d653a6cb2ed2fd0617aded
bhowden <bhowden@fs.fed.us>
2019-02-12 15:58:53 -0500

    IT equipment table

--- src/main/java/gov/usda/fs/ead/boss/model/ITEquipment.java
+++ src/main/java/gov/usda/fs/ead/boss/model/ITEquipment.java
@@ -2,12 +2,15 @@ package gov.usda.fs.ead.boss.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
@@ -19,6 +22,28 @@ public class ITEquipment implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
+    @Column(name = "Category", nullable = true)
+    private String category;
+    
+    @Column(name = "EquipmentName", nullable = false)
+    private String equipmentName;
+    
+    @Column(name = "Make", nullable = true)
+    private String make;
+    
+    @Column(name = "Type", nullable = true)
+    private String type;
+    
+    @Column(name = "ModelNumber", nullable = true)
+    private String modelNumber;
+    
+    @Column(name = "SerialNumber", nullable = true)
+    private String serialNumber;
+    
+    @Temporal(javax.persistence.TemporalType.DATE)
+    @Column(name = "AcquisitionDate", nullable = true)
+    private Date acquisitionDate;
     
 
     /**
@@ -33,6 +58,104 @@ public class ITEquipment implements Serializable {
      */
     public void setId(Long id) {
         this.id = id;
+    }
+
+    /**
+     * @return the category
+     */
+    public String getCategory() {
+        return category;
+    }
+
+    /**
+     * @param category the category to set
+     */
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    /**
+     * @return the equipmentName
+     */
+    public String getEquipmentName() {
+        return equipmentName;
+    }
+
+    /**
+     * @param equipmentName the equipmentName to set
+     */
+    public void setEquipmentName(String equipmentName) {
+        this.equipmentName = equipmentName;
+    }
+
+    /**
+     * @return the make
+     */
+    public String getMake() {
+        return make;
+    }
+
+    /**
+     * @param make the make to set
+     */
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    /**
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * @return the modelNumber
+     */
+    public String getModelNumber() {
+        return modelNumber;
+    }
+
+    /**
+     * @param modelNumber the modelNumber to set
+     */
+    public void setModelNumber(String modelNumber) {
+        this.modelNumber = modelNumber;
+    }
+
+    /**
+     * @return the serialNumber
+     */
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    /**
+     * @param serialNumber the serialNumber to set
+     */
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    /**
+     * @return the acquisitionDate
+     */
+    public Date getAcquisitionDate() {
+        return acquisitionDate;
+    }
+
+    /**
+     * @param acquisitionDate the acquisitionDate to set
+     */
+    public void setAcquisitionDate(Date acquisitionDate) {
+        this.acquisitionDate = acquisitionDate;
     }
     
 }
